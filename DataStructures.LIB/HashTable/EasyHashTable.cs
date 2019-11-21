3eff998ef10866f903f8fa73f9bd50e627cd31da
Victoria Berezina <navna3@gmail.com>
2019-11-21 19:25:17 +0300

    EasyHashTable added

---
+++ DataStructures.LIB/HashTable/EasyHashTable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace DataStructures.LIB.HashTable
+{
+    /// <summary>
+    /// Easy impl. Сollisions are not correctly resolved.
+    /// </summary>
+    public class EasyHashTable<T>
+    {
+        public T[] items;
+
+        public EasyHashTable(int size)
+        {
+            items = new T[size];
+        }
+
+        public void Add(T item)
+        {
+            var key = GetHash(item);
+            items[key] = item;
+        }
+
+        public bool Search(T item)
+        {
+            var key = GetHash(item);
+            return items[key].Equals(item);
+        }
+
+        private int GetHash(T item)
+        {
+            return item.ToString().Length % items.Length;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sbuilder = new StringBuilder();
+            return sbuilder.AppendJoin(' ', items).ToString();
+        }
+    }
+}
---
+++ DataStructures.TESTS/HashTableTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using DataStructures.LIB.HashTable;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataStructures.TESTS
+{
+    [TestClass]
+    public class HashTableTest
+    {
+        [TestMethod]
+        public void EasyHashTableAddTest()
+        {
+            var table = new EasyHashTable<int>(10);
+
+            table.Add(15);
+            table.Add(1);
+            table.Add(234);
+
+            Assert.AreEqual("0 1 15 234 0 0 0 0 0 0", table.ToString());
+        }
+
+        [TestMethod]
+        public void EasyHashTableSearchTest()
+        {
+            var table = new EasyHashTable<int>(10);
+
+            table.Add(15);
+            table.Add(1);
+            table.Add(234);
+
+            Assert.IsTrue(table.Search(15));
+            Assert.IsFalse(table.Search(14));
+        }
+    }
+}
